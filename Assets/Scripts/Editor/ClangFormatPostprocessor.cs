f301df12b9446bc8138fc1ea3385feee7004f061
Chris Helmich <kagekirin@gmail.com>
2021-12-14 13:18:41 +0900

    refactor: use ClangFormatSettings in ClangFormatPostprocessor

--- Assets/Scripts/Editor/ClangFormatPostprocessor.cs
+++ Assets/Scripts/Editor/ClangFormatPostprocessor.cs
@@ -18,6 +18,8 @@ namespace kagekirin.clangformat
 
         void OnPreprocessAsset()
         {
+            ClangFormatSettings clangFormatSettings      = ClangFormatSettings.GetOrCreateSettings();
+
             if (assetPath.EndsWith(".cs"))
             {
                 var fullPath = Path.Combine(RootPath, assetPath);
@@ -27,14 +29,8 @@ namespace kagekirin.clangformat
                 {
                     using (var process = new System.Diagnostics.Process())
                     {
-                        process.StartInfo.UseShellExecute = true;
-#if UNITY_EDITOR_WIN
-                        process.StartInfo.FileName = @"clang-format.exe";
-#elif UNITY_EDITOR_OSX
-                        process.StartInfo.FileName = @"/usr/local/bin/clang-format";
-#else // UNITY_EDITOR_LINUX
-                        process.StartInfo.FileName = @"/usr/local/bin/clang-format";
-#endif
+                        process.StartInfo.UseShellExecute        = true;
+                        process.StartInfo.FileName               = clangFormatSettings.m_InstallPath;
                         process.StartInfo.Arguments              = @"-i " + fullPath;
                         process.StartInfo.CreateNoWindow         = true;
                         process.StartInfo.RedirectStandardOutput = false;
